83c86a569edfe5d96500554c551aa0b355e54367
Ray-M05 <rachelmojena@gmail.com>
2024-03-16 00:57:27 -0400

    agregando objetos y ediciones
    visualizacion de las cartas y sus propiedades, ademas arreglos en los scripts

--- Assets/Scripts/Button action.cs
+++ Assets/Scripts/Button action.cs
@@ -6,27 +6,35 @@ public class Buttonaction : MonoBehaviour
 {
     public GameObject panel;
     public cardaction Carta;
-
+    
     public void invocacion() 
         //activa el menu de opciones cuando se presiona el boton de invocacion
     {
         gameObject.SetActive(false);
         panel.SetActive(true);
         panel.GetComponent<Buttonaction>().Carta = Carta;
+    
     }
 
     public void invocarCuerpoacuerpo()
     {
         Carta.invocar(1);
+        
+       
     }
 
     public void invocarDistancia()
     {
-        Carta.invocar(2);
+      Carta.invocar(2);
+       
     }
 
     public void invocarAsedio()
     {
         Carta.invocar(3);
+     
     }
+
+
+    
 }
--- Assets/Scripts/card action.cs
+++ Assets/Scripts/card action.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,10 +10,15 @@ public class cardaction : MonoBehaviour
     RawImage imagecard; //imagen de la carta actual
     hogwartsdeck mazo; //localizar el script deck
     public GameObject panel_opciones,panel_invocaciones; //inicializar el panel de menu de invocaciones
+    public int carta_mano;
+    public RawImage cardvisual;
+    public TextMeshPro cardescription;
     private void Start()
     {
         imagecard = GetComponent<RawImage>(); //inicializa imagen de la carta
         mazo = GameObject.FindGameObjectWithTag("Gryffindor").GetComponent<hogwartsdeck>(); //inicializa el mazo
+        cardvisual = GameObject.FindGameObjectWithTag("cardvisual").GetComponent<RawImage>();  //
+        cardescription = GameObject.FindGameObjectWithTag("cardescription").GetComponent<TextMeshPro>();
     }
 
     public void Opciones()  //funcion del boton de carta
@@ -20,51 +26,73 @@ public class cardaction : MonoBehaviour
         panel_opciones.SetActive(true);  //activa el panel de opciones cuando toca la carta
         panel_invocaciones.SetActive(false);
         panel_opciones.GetComponent<Buttonaction>().Carta = GetComponent<cardaction>();
+        cardvisual.texture = imagecard.texture;
     }
     public void invocar(int campo) //un numero que sera cuerpoacuerpo, distancia o asedio
     {
         if (imagecard.texture != null) //verifica que haya una imagen en la carta
         {
-            if(campo == 1)
+            if(campocarta("Cuerpoacuerpo",mazo.Hand, carta_mano))
             {
-                for (int i = 0; i < mazo.PosCuerpoacuerpo.Count; i++) //recorre pos cuerpoacuerpo
+                if (campo == 1)
                 {
-                    if (mazo.PosCuerpoacuerpo[i].texture == null) //verifica que las pos cuerpoacuerpo no tengan una carta
+                    for (int i = 0; i < mazo.PosCuerpoacuerpo.Count; i++) //recorre pos cuerpoacuerpo
                     {
-                        mazo.PosCuerpoacuerpo[i].texture = imagecard.texture; //pasa la imagen de la carta en pos cuerpoacuerpo
-                        imagecard.texture = null; //quita la carta de hand
-                        break;
+                        if (mazo.PosCuerpoacuerpo[i].texture == null) //verifica que las pos cuerpoacuerpo no tengan una carta
+                        {
+                            mazo.PosCuerpoacuerpo[i].texture = imagecard.texture; //pasa la imagen de la carta en pos cuerpoacuerpo
+                            imagecard.texture = null; //quita la carta de hand
+                            break;
+                        }
                     }
                 }
             }
 
-            if (campo == 2)
+            if (campocarta("Distancia", mazo.Hand, carta_mano))
             {
-                for (int i = 0; i < mazo.PosDistancia.Count; i++) //recorre pos distancia
+                if (campo == 2)
                 {
-                    if (mazo.PosDistancia[i].texture == null) //verifica que las pos distancia no tengan una carta
+                    for (int i = 0; i < mazo.PosDistancia.Count; i++) //recorre pos distancia
                     {
-                        mazo.PosDistancia[i].texture = imagecard.texture; //invocar la carta en distancia
-                        imagecard.texture = null; //quita la carta de hand
-                        break;
+                        if (mazo.PosDistancia[i].texture == null) //verifica que las pos distancia no tengan una carta
+                        {
+                            mazo.PosDistancia[i].texture = imagecard.texture; //invocar la carta en distancia
+                            imagecard.texture = null; //quita la carta de hand
+                            break;
+                        }
                     }
                 }
             }
 
-            if (campo == 3)
+            if (campocarta("Asedio", mazo.Hand,carta_mano))
             {
-                for (int i = 0; i < mazo.PosAsedio.Count; i++) //recorre pos asedio
+                if (campo == 3)
                 {
-                    if (mazo.PosAsedio[i].texture == null) //verifica que las pos asedio no tengan una carta
+                    for (int i = 0; i < mazo.PosAsedio.Count; i++) //recorre pos asedio
                     {
-                        mazo.PosAsedio[i].texture = imagecard.texture; //invocar la carta en asedio
-                        imagecard.texture = null; //quita la carta de hand
-                        break;
+                        if (mazo.PosAsedio[i].texture == null) //verifica que las pos asedio no tengan una carta
+                        {
+                            mazo.PosAsedio[i].texture = imagecard.texture; //invocar la carta en asedio
+                            imagecard.texture = null; //quita la carta de hand
+                            break;
+                        }
                     }
                 }
             }
+                
             
         }
     }
-
+    public bool campocarta(string verificacion, List<GameObject> hand,int carta_a_revisar)
+    {
+        for(int i = 0; i < hand[carta_a_revisar].GetComponent<Card>().campo.Length;i++)
+        {
+            if (hand[carta_a_revisar].GetComponent<Card>().campo[i].ToString() == verificacion)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
